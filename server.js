5e1980d83799da1badb1a4244c6625430980ce81
Varun Mehta <varun.mehta01@nagarro.com>
2019-05-23 10:35:21 +0530

    odified server path

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.static(__dirname + '/dist'));
 // For all GET requests, send back index.html
 // so that PathLocationStrategy can be used
 app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/campaign-organizer/index.html'));
   });
   
 
